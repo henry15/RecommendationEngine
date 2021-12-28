081e0d7da8925f0f1f541bc54be04f34aab9f927
Henry <henrychettiar@hotmail.com>
2021-12-28 16:21:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,5 +38,5 @@ def get_data(inp):
 #    suggestions = get_suggestions()
 #    return render_template('home.html',suggestions=suggestions)
 
-eel.start('index.html')
+eel.start('index.html', mode='edge')
 
